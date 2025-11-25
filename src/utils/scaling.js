20dcd1467efa31b91f5985ecaae11a8dd73aec6d
Anoop <herookie@tensi.org>
2025-11-25 09:38:51 +0530

    Fix ReferenceError: Property 'scale' doesn't exist in TopBrands component

--- src/utils/scaling.js
+++ src/utils/scaling.js
@@ -1,11 +1,28 @@
 import { Dimensions } from 'react-native'
 
-const { width, height } = Dimensions.get('window')
 // Guideline sizes are based on standard ~5" screen mobile device
 const guidelineBaseWidth = 350
 const guidelineBaseHeight = 680
-const scale = size => Math.round((width / guidelineBaseWidth) * size)
-const verticalScale = size => Math.round((height / guidelineBaseHeight) * size)
+
+const getDimensions = () => {
+  try {
+    return Dimensions.get('window')
+  } catch (error) {
+    // Fallback dimensions if Dimensions.get fails
+    return { width: 350, height: 680 }
+  }
+}
+
+const scale = size => {
+  const { width } = getDimensions()
+  return Math.round((width / guidelineBaseWidth) * size)
+}
+
+const verticalScale = size => {
+  const { height } = getDimensions()
+  return Math.round((height / guidelineBaseHeight) * size)
+}
+
 const moderateScale = (size, factor = 0.5) =>
   Math.round(size + (scale(size) - size) * factor)
 
